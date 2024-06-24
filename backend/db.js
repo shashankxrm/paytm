44ec32e5bffa66c6f311bf399708072cb2a8e0fc
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-06-24 15:47:07 +0530

    backend updated

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+// backend/db.js
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://shashankxrm:iNCORRECT%4045@cluster0.mwm4ybk.mongodb.net/");
+mongoose.connect("mongodb://localhost:27017/paytm")
 
-// Creating a Schema for Users
-
+// Create a Schema for Users
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -33,10 +33,22 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Creating a model from the schema
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
 
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
-	User
+	User,
+    Account
 };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
-const express = require("express");
+// backend/index.js
+const express = require('express');
+const cors = require("cors");
 const rootRouter = require("./routes/index");
+
 const app = express();
 
-// Requests to /api/v1 will be handled by rootRouter
-app.use("api/v1", rootRouter);  
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/v1", rootRouter);
 
+app.listen(3000);
